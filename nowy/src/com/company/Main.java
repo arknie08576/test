145c9bc7afa7e398ebbe847721d458d89aa021a0
arknie08576 <36695305+arknie08576@users.noreply.github.com>
2018-03-11 14:03:23 +0100

    trzeci1

--- nowy/src/com/company/Main.java
+++ nowy/src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
 
         System.out.println("Enter a password: ");
-	System.out.println("naranch bran ");
+	System.out.println("trzeci bran ");
 
 	// write your code here
     }
