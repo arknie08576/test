f17be9774155dee4d38e75503c218a2b73fafda9
arknie08576 <36695305+arknie08576@users.noreply.github.com>
2018-03-11 13:43:08 +0100

    First commit
    nowy java project

---
+++ nowy/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+        System.out.println("Enter a password: ");
+	// write your code here
+    }
+}
